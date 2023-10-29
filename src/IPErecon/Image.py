2fef4def17749bde9b766c2cd957bbf8712ca948
Michal Dymacek <216893@vutbr.cz>
2023-10-29 19:08:42 +0100

    notebook added to gitignore and load function in Image

--- src/IPErecon/Image.py
+++ src/IPErecon/Image.py
@@ -1,3 +1,30 @@
+"""Module containing the Image class. Image class takes care of loading and processing of
+images.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage
+import skimage.io as io
+from skimage import color
+
+class Image:
+    data = [] # results of surface coverage are saved here when save_surface() is called
+    speed = [] # results of speed of growth are saved here when speed_calculation() is called
+    roi = None
+
+    def __init__(self, image: np.ndarray, name: str):
+        self.images = {'image': image}
+        self.metadata = {'name': name}
+        self.results = {}
+    
+    @property
+    def image(self):
+        return self.images.get("image", None)
+    
+    @classmethod
+    def load_image(cls, path: str,name: str):
+        if not isinstance(path, str):
+            raise TypeError("Path must be a string.")
+        image = io.imread(path,as_gray=True)
+        return cls(image, name)
+        
